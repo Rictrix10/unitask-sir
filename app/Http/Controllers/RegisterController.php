511b42508f52bc9ce925cd60f23b3ab3de503082
Gonçalo Fonte <goncalofonte82@gmail.com>
2023-12-24 16:58:32 +0000

    nao consegui tirar o bug no register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -13,32 +13,33 @@ class RegisterController extends Controller
         $email = $request->input('email');
         $username = $request->input('username');
         $password = $request->input('password');
-        $phone_number = $request->input('phone_number', ''); // Valor padrão vazio se não fornecido
-        $address = $request->input('address', ''); // Valor padrão vazio se não fornecido
+        $phone_number = $request->input('phone_number', ''); 
+        $address = $request->input('address', ''); 
 
         // Validar entrada
-        if (empty($name) || empty($email) || empty($username) || empty($password)) {
-            return "Por favor, preencha todos os campos.";
+        if (!$request->filled(['name', 'email', 'username', 'password'])) {
+            return redirect()->route('login')->withErrors(['error2' => 'Por favor, preencha todos os campos!'])->withInput($request->all());
         }
 
         // Verificar se o usuário já existe
         $existingUser = User::where('username', $username)->orWhere('email', $email)->first();
 
         if ($existingUser) {
-            return "Nome de usuário ou email já está em uso. Escolha outros.";
+            return redirect()->route('login')->with('error2', 'Nome de usuário ou email já está em uso. Escolha outros.!');
         }
 
-        // Criar novo usuário
-        $newUser = new User();
-        $newUser->name = $name;
-        $newUser->email = $email;
-        $newUser->username = $username;
-        $newUser->password = $password; // Lembre-se de criptografar a senha no ambiente de produção
-        $newUser->phone_number = $phone_number;
-        $newUser->address = $address;
-        $newUser->save();
+        if(!$existingUser){
+            $newUser = new User();
+            $newUser->name = $name;
+            $newUser->email = $email;
+            $newUser->username = $username;
+            $newUser->password = $password; // Lembre-se de criptografar a senha no ambiente de produção
+            $newUser->phone_number = $phone_number;
+            $newUser->address = $address;
+            $newUser->save();
 
-        // Registro bem-sucedido
-        return redirect()->route('homepage'); // Redireciona para a página inicial
+            // Registro bem-sucedido
+            return redirect()->route('login'); // Redireciona para a página inicial
+        }
     }
 }
